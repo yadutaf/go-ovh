db89abbf34c6d50601946e7fd332ad1c5ec2d1f3
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2015-12-05 12:17:05 +0100

    fix: more reviews

--- ovh/client.go
+++ ovh/client.go
@@ -9,10 +9,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/user"
 	"strings"
 	"time"
 
-	"github.com/mitchellh/go-homedir"
 	"gopkg.in/ini.v1"
 )
 
@@ -42,6 +42,11 @@ type Client struct {
 	Timeout           time.Duration
 	timeDelta         int64
 	client            *http.Client
+
+	// sync.Once would consider init done, even in case of error
+	// running it multiple times/races are not issue. Hence a good
+	// old flag
+	timeDeltaDone bool
 }
 
 // APIResponse represents a response from OVH API
@@ -58,6 +63,15 @@ type APIError struct {
 	Message   string `json:"message"`
 }
 
+// Util: get user home
+func currentUserHome() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	return usr.HomeDir, nil
+}
+
 // NewDefaultClient returns an OVH API Client from external configuration
 func NewDefaultClient() (*Client, error) {
 	return NewClient("", "", "", "")
@@ -70,9 +84,11 @@ func NewEndpointClient(endpoint string) (*Client, error) {
 
 // NewClient returns an OVH API Client.
 func NewClient(endpointName, applicationKey, applicationSecret, consumerKey string) (*Client, error) {
-	// Load configuration files. Only load file from user home if home could be resolve
-	cfg, err := ini.Load("/etc/ovh.conf")
-	if home, err := homedir.Dir(); err == nil {
+	// Load configuration files by order of increasing priority. All configuration
+	// files are optional. Only load file from user home if home could be resolve
+	cfg := ini.Empty()
+	cfg.Append("/etc/ovh.conf")
+	if home, err := currentUserHome(); err == nil {
 		cfg.Append(home + "/.ovh.conf")
 	}
 	cfg.Append("./ovh.conf")
@@ -94,6 +110,7 @@ func NewClient(endpointName, applicationKey, applicationSecret, consumerKey stri
 		consumerKey = getConfigValue(cfg, endpointName, "consumer_key")
 	}
 
+	// Load real endpoint URL by name. If endpoint contains a '/', consider it as a URL
 	var endpoint Endpoint
 	if strings.Contains(endpointName, "/") {
 		endpoint = Endpoint(endpointName)
@@ -111,22 +128,8 @@ func NewClient(endpointName, applicationKey, applicationSecret, consumerKey stri
 		applicationSecret: applicationSecret,
 		consumerKey:       consumerKey,
 		Timeout:           timeout,
-		timeDelta:         0,
 		client:            &http.Client{},
 	}
-
-	// Account for clock delay with API in signatures
-	timeDelta, err := client.GetUnAuth("/auth/time")
-	if err != nil {
-		return nil, err
-	}
-
-	var serverTime int64
-	err = json.Unmarshal(timeDelta.Body, &serverTime)
-	if err != nil {
-		return nil, err
-	}
-	client.timeDelta = time.Now().Unix() - serverTime
 
 	return client, nil
 }
@@ -219,10 +222,31 @@ func (c *Client) DeleteUnAuth(path string) (*APIResponse, error) {
 // Low Level Helpers
 //
 
+// Account for clock delay in API in signatures
+func (c *Client) getTimeDelta() int64 {
+	if c.timeDeltaDone != true {
+		// Attempt to get timeDelta or fallback on 0
+		timeDelta, err := c.GetUnAuth("/auth/time")
+		if err != nil {
+			return 0
+		}
+
+		// Attempt to load timeDelta or fallback on 0
+		var serverTime int64
+		err = json.Unmarshal(timeDelta.Body, &serverTime)
+		if err != nil {
+			return 0
+		}
+		c.timeDelta = time.Now().Unix() - serverTime
+		c.timeDeltaDone = true
+	}
+	return c.timeDelta
+}
+
 // Call calls OVH's API and signs the request if ``needAuth`` is ``true``
 func (c *Client) Call(method, path string, data interface{}, needAuth bool) (*APIResponse, error) {
 	target := fmt.Sprintf("%s%s", c.endpoint, path)
-	timestamp := time.Now().Unix() - c.timeDelta
+	timestamp := time.Now().Unix() - c.getTimeDelta()
 
 	var body []byte
 	var err error
@@ -271,10 +295,14 @@ func (c *Client) Call(method, path string, data interface{}, needAuth bool) (*AP
 	}
 	defer r.Body.Close()
 
-	response := &APIResponse{}
-	response.StatusCode = r.StatusCode
-	response.Status = r.Status
-	response.Body, err = ioutil.ReadAll(r.Body)
+	response, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
 
-	return response, nil
+	return &APIResponse{
+		StatusCode: r.StatusCode,
+		Status:     r.Status,
+		Body:       response,
+	}, nil
 }
